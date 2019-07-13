ced27aa9aa52489295e54ac96f5d4940d6bf22b8
Zinko Vyacheslav <zslavman@gmail.com>
2019-07-13 21:07:06 +0300

    add checkBox for favorites

--- PodcastsApp/Views/FavoritePodcastCell.swift
+++ PodcastsApp/Views/FavoritePodcastCell.swift
@@ -25,12 +25,29 @@ class FavoritePodcastCell: UICollectionViewCell {
 		label.textColor = .lightGray
 		return label
 	}()
+	private let checkBox: UIView = {
+		let box = UIView()
+		box.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+		box.layer.cornerRadius = 8
+		box.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1).cgColor
+		box.layer.borderWidth = 1
+		box.translatesAutoresizingMaskIntoConstraints = false
+		//box.isHidden = true
+		return box
+	}()
+	private let checkMark: UIImageView = {
+		let checkmark = UIImageView(image: #imageLiteral(resourceName: "check-mark"))
+		checkmark.tintColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+		checkmark.translatesAutoresizingMaskIntoConstraints = false
+		checkmark.contentMode = .scaleAspectFit
+		return checkmark
+	}()
 	public var podcast: Podcast!
 	
 	override var isSelected: Bool {
 		didSet {
 			if isSelected {
-				imageView.alpha = 0.15
+				imageView.alpha = 0.3
 			}
 			else {
 				imageView.alpha = 1
@@ -52,6 +69,8 @@ class FavoritePodcastCell: UICollectionViewCell {
 		stackView.axis = .vertical
 		stackView.translatesAutoresizingMaskIntoConstraints = false
 		addSubview(stackView)
+		addSubview(checkBox)
+		addSubview(checkMark)
 		
 		NSLayoutConstraint.activate([
 			imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),// picture must be square
@@ -59,6 +78,16 @@ class FavoritePodcastCell: UICollectionViewCell {
 			stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
 			stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
 			stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+			
+			checkBox.topAnchor.constraint(equalTo: topAnchor, constant: 7),
+			checkBox.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -7),
+			checkBox.widthAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 0.35),
+			checkBox.heightAnchor.constraint(equalTo: checkBox.widthAnchor),
+			
+			checkMark.centerXAnchor.constraint(equalTo: checkBox.centerXAnchor),
+			checkMark.centerYAnchor.constraint(equalTo: checkBox.centerYAnchor),
+			checkMark.widthAnchor.constraint(equalTo: checkBox.widthAnchor, multiplier: 0.9),
+			checkMark.heightAnchor.constraint(equalTo: checkMark.widthAnchor),
 		])
 	}
 	
